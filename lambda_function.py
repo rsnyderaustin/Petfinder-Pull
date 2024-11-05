485b557263a50243dc4c661c1ecddf0e21f68c00
rsnyderaustin <rsnyder.austin@gmail.com>
2024-11-04 23:06:10 -0600

    Updated lambda_function to reflect recent changes

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,7 @@
-import boto3
 from botocore.config import Config
 import os
 
-from dynamodb_management import DynamodbManager
-import petfinder_api
-from petfinder_api import PetfinderApiManager
+import program_management
 
 retry_config = Config(
     retries={
@@ -25,28 +22,15 @@ def lambda_handler():
     data_url = os.getenv('PF_DATA_REQUEST_URL')
     token_url = os.getenv('PF_ACCESS_TOKEN_URL')
     dynamodb_table_name = os.getenv('PF_DYNAMODB_TABLE_NAME')
-    pf_api_manager = PetfinderApiManager(
+    
+    prog_manager = program_management.ProgramManager(
         api_key=api_key,
         secret_key=secret_key,
-        data_url=data_url,
-        token_url=token_url
+        data_request_url=data_url,
+        token_url=token_url,
+        dynamodb_table_name=dynamodb_table_name
     )
-
-    dynamodb_manager = DynamodbManager(table_name=dynamodb_table_name)
-
-    animals = {}
-
-    dog_data = pf_api_manager.get_from_api(category='animals',
-                                           type='dog')
-    for id_, data in dog_data.items():
-        new_animal = petfinder_api.create_animal(**data)
-        animals[id_] = new_animal
-
-    cat_data = pf_api_manager.get_from_api(category='animals',
-                                           type='cat')
-    for id_, data in cat_data.items():
-        new_animal = petfinder_api.create_animal(**data)
-        animals[id_] = new_animal
+    prog_manager.update_database()
 
 
 if not in_aws:
